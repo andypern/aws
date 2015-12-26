ec7d326dbcb3a97b8b64c91551028e3b0da5bacb
Andy Pernsteiner <andypern@gmail.com>
2015-12-26 07:49:04 -0500

    bug chasing

--- instance_inventory.py
+++ instance_inventory.py
@@ -137,7 +137,7 @@ def check_tags(instance):
 			phat_hash['user_tag_updates'].append(instance.key_name)
 	try:
 		name = instance.tags['Name']
-		print name
+		print "hello"
 	except KeyError:
 		if instance.key_name is None:
 			print "null key name!"
